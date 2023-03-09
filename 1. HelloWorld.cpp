ae2b57e05a9b63f5beea783fc5e19e33ee04255f
Alex <mwppl123@gmail.com>
2023-03-09 10:17:21 +0100

    Innit + int operators

---
+++ 1. HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+    cout <<"Witaj swiecie\n";
+    int a,b,c;
+    a = 7;
+    b = 6/3;
+    c = a + b;
+    cout << "Dodawanie " << c << endl;
+    c = a - b;
+    cout << "Odejmowanie " << c << endl;
+    c = a * b;
+    cout << "Iloczyn " << c << endl;
+    c = a / b;
+    cout << "Iloraz " << c << endl;
+    c = a % b;
+    cout << "Reszta " << c << endl;
+
+}
