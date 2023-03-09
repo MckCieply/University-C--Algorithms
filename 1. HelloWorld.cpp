10ec70662ab21db7a14f224b229f1458737ba501
Alex <mwppl123@gmail.com>
2023-03-09 10:20:43 +0100

    Featuring floats

--- 1. HelloWorld.cpp
+++ 1. HelloWorld.cpp
@@ -2,18 +2,31 @@
 using namespace std;
 int main(int argc, char* argv[]){
     cout <<"Witaj swiecie\n";
+    cout << "\n INTS " << endl;
     int a,b,c;
     a = 7;
     b = 6/3;
     c = a + b;
-    cout << "Dodawanie " << c << endl;
+    cout << "Dodawanie: " << c << endl;
     c = a - b;
-    cout << "Odejmowanie " << c << endl;
+    cout << "Odejmowanie: " << c << endl;
     c = a * b;
-    cout << "Iloczyn " << c << endl;
+    cout << "Iloczyn: " << c << endl;
     c = a / b;
-    cout << "Iloraz " << c << endl;
+    cout << "Iloraz: " << c << endl;
     c = a % b;
-    cout << "Reszta " << c << endl;
+    cout << "Reszta: " << c << endl;
 
+    cout << "\n FLOATS " << endl;
+    float x, y, z;
+    x = 5.6;
+    y = 2.5;
+    z = x + y;
+    cout << "Dodawanie: " << z << endl;
+    z = x - y;
+    cout << "Odejmowanie: " << z << endl;
+    z = x * y;
+    cout << "Iloczyn: " << z << endl;
+    z = x / y;
+    cout << "Iloraz: " << z << endl;
 }
